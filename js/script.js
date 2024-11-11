380f0775421119e6ef50d4715481ac9581ba6185
quinnnguyenn <quinn.nguyen@bellevuecollege.edu>
2024-11-10 22:47:37 -0800

    commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("What's a number between 1 and 10? ", function(number) {
-  randomNumber = Math.floor(Math.random() * 10) + 1;
-  console.log(randomNumber);
-  rl.close();
-});
+var number = prompt("hi");
