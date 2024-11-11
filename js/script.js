03b37e9d0840c18beac6e6e10e404f0be831feb2
quinnnguyenn <quinn.nguyen@bellevuecollege.edu>
2024-11-10 23:41:46 -0800

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 randomNumber = Math.floor(Math.random() * 10) +1;
-document.getElementById("demo").innerHTML = randomNumber;
-
 let number = prompt("What's a number between 1 and 10")
 
 if (number == randomNumber) {
